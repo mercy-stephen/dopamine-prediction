1439669959a0ac9ac0c670f9503c7192a3ab7112
mercy-stephen <81503326+mercy-stephen@users.noreply.github.com>
2021-11-16 09:55:21 +0530

    prediction

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ import os
 import base64
 import pickle
 
+# Molecular descriptor calculator
+def desc_calc():
+    # Performs the descriptor calculation
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    os.remove('molecule.smi')
+
 # File download
 def filedownload(df):
     csv = df.to_csv(index=False)
